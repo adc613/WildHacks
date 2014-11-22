36c4679d0a56d0df55d0106c9439c8c48aa34ed6
Adam Collins <adc82@case.edu>
2014-11-22 16:40:24 -0600

    Finished the users/views.py file so put in a login, logout, and sign up view.

--- wildHacks/users/views.py
+++ wildHacks/users/views.py
@@ -1,3 +1,94 @@
 from django.shortcuts import render
+from django.contrib import messages
+from djanog.contrib.auth import login, logout
+from djanog.contrib.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import View
 
-# Create your views here.
+from .forms import UserCreationForm
+from .models import Profile
+
+def logout_view(request):
+    """
+    Logouts user and redirects to the hompage
+    """
+    logout(request)
+    messages.success(request,
+            """ It's okay that you logged out. I don't like you either. """
+            )
+    return HttpResponseRedirect(reverse('home'))
+
+class SignUpView(View):
+    """
+    Has the GET and POST methods for the signing up to be a user
+    """
+    template_name = 'sign_up.html'
+    form UserCreationForm
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST or None)
+        
+        if form.is_valid():
+            save_it = form.save(commit=True)
+            save_it.save()
+            messages.success(request, 
+                    """
+                    Thanks for joining. This could possibly be the most life
+                    changing event of your young life.
+                    """
+                    )
+            return HttpResponseRedirect(reverse('thanks'))
+        else
+            messages.error(request,
+                    """
+                    There seems to be an error with your application please try
+                    again.
+                    """
+                    )
+            return HttpReposeRedirect(reverse('users:sign_up'))
+
+class LoginView(View):
+    """
+    Logs user into the website
+    """
+    template_name = 'users/login.html'
+
+    def get(self, request):
+        return render(request, self.template_name, {})
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST['emai']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            message.success(request, 
+                    """
+                    Seems someone made a mistake please try again
+                    """
+                    )
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            if user.is_active:
+                login(request, user)
+                messages.success(request,
+                        """
+                        You did it! You successfully logged in. I bet your
+                        mother would be so proud of you right now.
+                        """
+                        )
+                return HttpResponseRedirect(reverse('thanks'))
+            else:
+                messages.success(request,
+                        """
+                        Looks like your not considered an active user.
+                        Please leave no one wants you here. 
+                        """
+                        )
+                return HttpResponseRedirect(reverse('thanks'))
+
+
