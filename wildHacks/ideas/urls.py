04750b2bba2c0abc1e2ee9ceaab94dee3ec8d6dd
Adam Collins <adc82@case.edu>
2014-11-23 11:37:09 -0600

    please work

--- wildHacks/ideas/urls.py
+++ wildHacks/ideas/urls.py
@@ -4,12 +4,8 @@ from .views import *
 
 urlpatterns = patterns('',
         url(r'^idea/(?P<pk>\d+)/$', IdeaDetailView.as_view(), name='detail'),
+        url(r'^like/(?P<pk>\d+)/$', like_view, name='like'),
+        url(r'^dislike/(?P<pk>\d+)/$', dislike_view, name='dislike'),
         url(r'^idea_list/$', IdeaListView.as_view(), name='list'),
         url(r'^create/$', IdeaCreationView.as_view(), name='create'),
-
-        # Generic view to vote on Link objects
-    	(r'^links/(?P<object_id>\d+)/(?P<direction>up|down|clear)vote/?$',
-        vote_on_object, dict(model=Link, template_object_name='link',
-            template_name='kb/link_confirm_vote.html',
-            allow_xmlhttprequest=True)),
         )
